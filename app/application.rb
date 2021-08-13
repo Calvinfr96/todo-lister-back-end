c08690450eaa203e90ef65a3f373080754e40c6e
calvin <calvin.francis96@outlook.com>
2021-08-13 17:44:50 -0400

    Remove previous reference and add reference to categories in User Index route

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     #Index
     if req.path == '/users' && req.get?
-      return [200, {'Content-Type' => 'application/json'}, [User.all.to_json]]
+      return [200, {'Content-Type' => 'application/json'}, [User.all.as_json(include: :categories).to_json]]
     end
 
     #Create
@@ -110,7 +110,7 @@ class Application
 
     #Index
     if req.path == '/tasks' && req.get?
-      return [200, {'Content-Type' => 'application/json'}, [Task.all.as_json(include: :category).to_json]]
+      return [200, {'Content-Type' => 'application/json'}, [Task.all.to_json]]
     end
 
     #Create
